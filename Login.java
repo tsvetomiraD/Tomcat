6fe549838aab99b594ecab532fb1e8b634282007
tsvetomiraD <95402102+tsvetomiraD@users.noreply.github.com>
2022-12-06 10:03:38 +0200

    Create Login.java

---
+++ Login.java
@@ -0,0 +1,30 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Login extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        Dao d = new Dao();
+        PrintWriter out = response.getWriter();
+
+        if (d.validUser(username, password)) {
+            HttpSession session = request.getSession(true);
+            session.setAttribute("username", username);
+            out.println("Welcome " + username);
+        }
+        else {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            out.println("Forbidden");
+        }
+    }
+}
